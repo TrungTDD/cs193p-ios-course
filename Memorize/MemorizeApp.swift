7f60dc807e50758e0ea87e39a8bb1e2bac8e2f07
trungtdd <duytrungdng@gmail.com>
2021-09-08 23:15:44 +0700

    First commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by TrungTruong on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
